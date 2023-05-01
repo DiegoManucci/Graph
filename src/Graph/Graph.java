1f2ceb6a97fb52be8f10ccba2e1aafa652b0366b
DiegoManucci <diegobizzotto2003@gmail.com>
2023-05-01 17:16:35 -0300

    Avaliação Parcial

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,7 +1,6 @@
 package Graph;
 
-import java.util.ArrayList;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Graph {
@@ -81,5 +80,99 @@ public class Graph {
 	}
 	public void setEdgeValue(Integer vertex1, Integer vertex2, Integer value) {
 		 findEdge(vertex1, vertex2).setValue(value);
+	}
+
+	public ArrayList<Vertex> caminho(Integer start, Integer end) {
+		ArrayList<Vertex> path = new ArrayList<>();
+		HashSet<Vertex> visited = new HashSet<>();
+		Vertex startVertex = findVertex(start);
+		Vertex endVertex = findVertex(end);
+
+		if (startVertex == null || endVertex == null) {
+			return path;
+		}
+
+		LinkedList<Vertex> queue = new LinkedList<>();
+		HashMap<Vertex, Vertex> parentMap = new HashMap<>();
+		queue.add(startVertex);
+		visited.add(startVertex);
+
+		while (!queue.isEmpty()) {
+			Vertex current = queue.poll();
+			if (current == endVertex) {
+				Vertex node = endVertex;
+				while (node != null) {
+					path.add(node);
+					node = parentMap.get(node);
+				}
+				Collections.reverse(path);
+				break;
+			}
+			for (Edge edge : edgeList) {
+				if (edge.getVertex1() == current && !visited.contains(edge.getVertex2())) {
+					visited.add(edge.getVertex2());
+					queue.add(edge.getVertex2());
+					parentMap.put(edge.getVertex2(), current);
+				}
+				else if (edge.getVertex2() == current && !visited.contains(edge.getVertex1())) {
+					visited.add(edge.getVertex1());
+					queue.add(edge.getVertex1());
+					parentMap.put(edge.getVertex1(), current);
+				}
+			}
+		}
+
+		return path;
+	}
+
+	public Graph uniao(Graph other) {
+		Graph union = new Graph();
+		HashSet<Vertex> seenVertices = new HashSet<>();
+
+		for (Vertex vertex : this.vertexList) {
+			union.addVertex(vertex.getValue());
+			seenVertices.add(vertex);
+		}
+		for (Vertex vertex : other.vertexList) {
+			if (!seenVertices.contains(vertex)) {
+				union.addVertex(vertex.getValue());
+				seenVertices.add(vertex);
+			}
+		}
+
+		for (Edge edge : this.edgeList) {
+			Vertex v1 = edge.getVertex1();
+			Vertex v2 = edge.getVertex2();
+			union.addEdge(edge.getValue(), v1.getValue(), v2.getValue());
+		}
+		for (Edge edge : other.edgeList) {
+			Vertex v1 = edge.getVertex1();
+			Vertex v2 = edge.getVertex2();
+			union.addEdge(edge.getValue(), v1.getValue(), v2.getValue());
+		}
+
+		return union;
+	}
+
+	public Graph interseccao(Graph other) {
+		Graph intersection = new Graph();
+		HashSet<Vertex> seenVertices = new HashSet<>();
+
+		for (Vertex vertex : this.vertexList) {
+			if (other.findVertex(vertex.getValue()) != null) {
+				intersection.addVertex(vertex.getValue());
+				seenVertices.add(vertex);
+			}
+		}
+
+		for (Edge edge : this.edgeList) {
+			Vertex v1 = edge.getVertex1();
+			Vertex v2 = edge.getVertex2();
+			if (seenVertices.contains(v1) && seenVertices.contains(v2)) {
+				intersection.addEdge(edge.getValue(), v1.getValue(), v2.getValue());
+			}
+		}
+
+		return intersection;
 	}
 }
--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -1,29 +1,62 @@
 package Graph;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-		Graph graph = new Graph();
-		
-		graph.addVertex(1);
-		graph.addVertex(2);
-		graph.addVertex(3);
-		graph.addVertex(4);
-		graph.addVertex(5);
-	
-		graph.addEdge(1, 1, 2);
-		graph.addEdge(2, 1, 3);
-		graph.addEdge(3, 1, 4);
-		
-		graph.removeEdge(1, 4);
-		
-		graph.removeVertex(1);
-		
-		graph.neighbors(1);
-		
-		System.out.println(graph.adjacent(2, 3));
-		System.out.println(graph.adjacent(1, 4));
+		Graph graph1 = new Graph();
+		Graph graph2 = new Graph();
 
-		graph.printVertexList();
-		graph.printEdgeList();
+		graph1.addVertex(1);
+		graph1.addVertex(2);
+		graph1.addVertex(3);
+		graph1.addVertex(4);
+
+		graph1.addEdge(1, 1, 2);
+		graph1.addEdge(2, 1, 3);
+		graph1.addEdge(3, 2, 3);
+		graph1.addEdge(4, 3, 4);
+		graph1.addEdge(5, 4, 2);
+
+		graph2.addVertex(3);
+		graph2.addVertex(4);
+		graph2.addVertex(5);
+		graph2.addVertex(6);
+
+		graph2.addEdge(1, 3, 4);
+		graph2.addEdge(2, 4, 5);
+		graph2.addEdge(3, 5, 6);
+
+		ArrayList<Vertex> path = graph1.caminho(1, 4);
+		if (path != null) {
+			System.out.print("Path from 1 to 4: ");
+			for (Vertex vertex : path) {
+				System.out.print(vertex.getValue() + " ");
+			}
+			System.out.println();
+		} else {
+			System.out.println("There is no path from 1 to 4.");
+		}
+
+		path = graph1.caminho(2, 5);
+		if (path != null) {
+			System.out.print("Path from 2 to 5: ");
+			for (Vertex vertex : path) {
+				System.out.print(vertex.getValue() + " ");
+			}
+			System.out.println();
+		} else {
+			System.out.println("There is no path from 2 to 5.");
+		}
+
+		Graph union = graph1.uniao(graph2);
+		System.out.println("Union of G1 and G2:");
+		union.printVertexList();
+		union.printEdgeList();
+
+		Graph intersection = graph1.interseccao(graph2);
+		System.out.println("Intersection of G1 and G2:");
+		intersection.printVertexList();
+		intersection.printEdgeList();
 	}
 }
